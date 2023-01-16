a5334def1d8d5e883df2a595257069b21a6939f1
mosfeqanik <mosfeqanik01@gmail.com>
2023-01-16 15:27:59 +0600

    all app strings added

---
+++ lib/app/utils/AllStrings.dart
@@ -0,0 +1,14 @@
+//All Apis endpoint
+class AllStrings {
+  AllStrings._();
+
+  static const String AppTittle = "Git repositories";
+  static const String HelloRichText = "Hello ";
+  static const String AdminText =  "Admin";
+  static const String WelcomeBack =  ", welcome back!";
+  static const String listHeadingName =  "Flutter Repository";
+  static const String RepositoryDetail =  "Repository Detail";
+  static const String RepositoryDescription =  "Repository Description";
+  static const String RepositoryName =  "Repository Name";
+  static const String Information =  "Information";
+}
