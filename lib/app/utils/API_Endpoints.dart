3efbba7496045f714c4d61fd0f1a51ad06deb83c
mosfeqanik <mosfeqanik01@gmail.com>
2023-01-16 15:28:13 +0600

    all Api Endpoint strings added

--- lib/app/utils/API_Endpoints.dart
+++ lib/app/utils/API_Endpoints.dart
@@ -1,8 +1,9 @@
-
-
+//All Apis endpoint
 class APIs {
   APIs._();
+
   // https://api.github.com/search/repositories?q={Flutter}{&page,per_page,sort,order}
   static const String baseUrl = "https://api.github.com/search/repositories";
-  static const String getFlutterRepoUrl = "$baseUrl?q={Flutter}{&page,per_page,sort,order}";
+  static const String getFlutterRepoUrl =
+      "$baseUrl?q={Flutter}{&page,per_page,sort,order}";
 }
