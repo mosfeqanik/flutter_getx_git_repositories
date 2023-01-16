dde5180f80ab58116aca27b0b27ed804528e8f9a
mosfeqanik <mosfeqanik01@gmail.com>
2023-01-16 15:32:19 +0600

    list reformated

--- lib/app/modules/home/components/ListTileWidget.dart
+++ lib/app/modules/home/components/ListTileWidget.dart
@@ -2,7 +2,6 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
 class ListTileWidget extends StatelessWidget {
   const ListTileWidget(
       {Key? key,
