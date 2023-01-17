2bdfa9775fbb6f75b54da2a09c88fb488b16005c
mosfeqanik <mosfeqanik01@gmail.com>
2023-01-17 11:50:10 +0600

    sortByDate & sortByStar added 7. Selected sorting option persists in further app sessions. Added

--- lib/app/data/services/cache_manager.dart
+++ lib/app/data/services/cache_manager.dart
@@ -13,8 +13,38 @@ mixin CacheManager {
   String? getGitDetailsData() {
     return box.read(CacheManagerKey.GitDetails.toString());
   }
+  Future<bool> saveGitDataSortByDate({
+    String? GitDetails,
+  }) async {
+    await box.write(
+        CacheManagerKey.IsGitDetailsSortBy.toString(), "IsGitDetailsSortByDate");
+    await box.write(
+        CacheManagerKey.GitDetailsSortByDate.toString(), GitDetails);
+    return true;
+  }
+  String? getGitSortByDate() {
+    return box.read(CacheManagerKey.GitDetailsSortByDate.toString());
+  }
+  String? IsgetGitSortBy() {
+    return box.read(CacheManagerKey.IsGitDetailsSortBy.toString());
+  }
+  Future<bool> saveGitDataSortByStar({
+    String? GitDetails,
+  }) async {
+    await box.write(
+        CacheManagerKey.IsGitDetailsSortBy.toString(), "IsGitDetailsSortByStar");
+    await box.write(
+        CacheManagerKey.GitDetailsSortByStar.toString(), GitDetails);
+    return true;
+  }
+  String? getGitSortByStar() {
+    return box.read(CacheManagerKey.GitDetailsSortByStar.toString());
+  }
 }
 
 enum CacheManagerKey {
   GitDetails,
+  IsGitDetailsSortBy,
+  GitDetailsSortByDate,
+  GitDetailsSortByStar,
 }
--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -8,24 +8,43 @@ import '../../splash/get_git_repositories_model.dart';
 
 class HomeController extends GetxController with CacheManager {
   var getGitData = "".obs;
+  var getGitDataSortByDate = "".obs;
+  var IsgetGitDataSortBy = "".obs;
+  var getGitDataSortByStar = "".obs;
   var getGitRepositories = GetGitRepositories().obs;
   var itemsList =<Items>[].obs;
 
   loadGitDetailsData() {
     getGitData.value = getGitDetailsData()!;
-    getGitRepositories.value =
-        GetGitRepositories.fromJson(jsonDecode(getGitData.value));
-    itemsList.value= getGitRepositories.value.items!;
+    getGitDataSortByStar.value = getGitSortByStar()??"";
+    getGitDataSortByDate.value = getGitSortByDate()??"";
+
+    IsgetGitDataSortBy.value = IsgetGitSortBy()??"";
+    if(IsgetGitDataSortBy.value=="IsGitDetailsSortByStar"){
+      itemsList.value =  parseItems(getGitDataSortByStar.value);
+    }else if(IsgetGitDataSortBy.value=="IsGitDetailsSortByDate"){
+      itemsList.value =  parseItems(getGitDataSortByDate.value);
+    }else{
+      getGitRepositories.value = GetGitRepositories.fromJson(jsonDecode(getGitData.value));
+      itemsList.value= getGitRepositories.value.items!;
+    }
+
+
+
   }
   sortByDate(){
     itemsList.sort((a, b){
       return DateTime.parse(b.updatedAt!).compareTo(DateTime.parse(a.updatedAt!));
     });
+    String jsonItemList = jsonEncode(itemsList);
+    saveGitDataSortByDate(GitDetails: jsonItemList);
   }
   sortByStar(){
     itemsList.sort((a, b){
       return b.stargazersCount!.compareTo(a.stargazersCount!);
     });
+    String jsonItemList = jsonEncode(itemsList);
+    saveGitDataSortByStar(GitDetails: jsonItemList);
   }
   
   // dateformatter
@@ -34,5 +53,11 @@ class HomeController extends GetxController with CacheManager {
     final DateFormat formatter = DateFormat('MMMM dd yyyy hh:mm:ss aaa');
     final String formatted = formatter.format(inputDateTime);
     return formatted;
+  }
+
+  List<Items> parseItems(String responseBody) {
+    final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
+
+    return parsed.map<Items>((json) => Items.fromJson(json)).toList();
   }
 }
